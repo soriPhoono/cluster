900b7f2edd7084d1475fd14902622d948feccf01
soriphoono <soriphoono@gmail.com>
2026-01-18 17:26:23 -0600

    Updated entrypoint secrets loading mechanism

---
+++ docker/admin/downloads/tailscale/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Read the secret file and export it as the auth key
+if [ -f /run/secrets/tailscale_authkey ]; then
+    TS_AUTHKEY=$(cat /run/secrets/tailscale-authkey)
+    export TS_AUTHKEY
+fi
+
+# Execute the original Tailscale entrypoint
+exec /usr/local/bin/containerboot "$@"
