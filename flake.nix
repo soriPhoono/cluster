8048c92bee6127e5df474403ffad279d01fbb8d7
soriphoono <soriphoono@gmail.com>
2026-02-26 20:08:46 -0600

    feat: added basic generated sops encrypted talos config

--- flake.nix
+++ flake.nix
@@ -88,61 +88,99 @@
         };
 
         apps = {
-          launch-development-cluster = { type = "app"; program = "${pkgs.writeShellApplication {
-            name = "launch-development-cluster.sh";
-            runtimeInputs = [
-              pkgs.docker
-              pkgs.talosctl
-              pkgs.kubectl
-              pkgs.fluxcd
-            ];
-            text = ''
-              # 1. Check if talosconfig exists (optional but recommended)
-              if [[ -z "$TALOSCONFIG" ]]; then
-                export TALOSCONFIG="$HOME/.talos/config"
-              fi
+          unseal = {
+            type = "app";
+            program = "${pkgs.writeShellApplication {
+              name = "unseal.sh";
+              runtimeInputs = with pkgs; [
+                talosctl
+                kubectl
+                fluxcd
+              ];
+              text = ''
+                sops --decrypt --in-place ./talos/talosconfig
+                sops --decrypt --in-place ./talos/controlplane.yaml
+                sops --decrypt --in-place ./talos/worker.yaml
+              '';
+            }}/bin/unseal.sh";
+          };
+          seal = {
+            type = "app";
+            program = "${pkgs.writeShellApplication {
+              name = "seal.sh";
+              runtimeInputs = with pkgs; [
+                git
 
-              QEMU_RUNNING=$(pgrep -f "qemu-system-x86_64.*talos" || true)
+                sops
 
-              # If there are no qemu PIDs with talos
-              if [ -z "$QEMU_RUNNING" ]; then
-                echo "No talos containers found. Creating a new cluster..."
-                sudo -E talosctl cluster create qemu
-
-                if [ -z "$GITHUB_TOKEN" ]; then
-                  echo "WARNING: GITHUB_TOKEN not found. flux bootstrap may fail if token-auth is required."
+                talosctl
+                kubectl
+                fluxcd
+              ];
+              text = ''
+                sops --encrypt --in-place ./talos/talosconfig
+                sops --encrypt --in-place ./talos/controlplane.yaml
+                sops --encrypt --in-place ./talos/worker.yaml
+              '';
+            }}/bin/seal.sh";
+          };
+          test-blackbox = {
+            type = "app";
+            program = "${pkgs.writeShellApplication {
+              name = "launch-development-cluster.sh";
+              runtimeInputs = with pkgs; [
+                talosctl
+                kubectl
+                fluxcd
+              ];
+              text = ''
+                # 1. Check if talosconfig exists (optional but recommended)
+                if [[ -z "$TALOSCONFIG" ]]; then
+                  export TALOSCONFIG="$HOME/.talos/config"
                 fi
 
-                echo "Cluster created successfully, initializing FluxCD inside newly created docker cluster"
-                flux bootstrap github \
-                  --token-auth \
-                  --owner=soriPhoono \
-                  --repository=cluster \
-                  --branch="$(git rev-parse --abbrev-ref HEAD)" \
-                  --path=k8s/clusters/testing/ \
-                  --personal --verbose
+                QEMU_RUNNING=$(pgrep -f "qemu-system-x86_64.*talos" || true)
 
-                if [ -n "$CLUSTER_AGE_KEY" ]; then
-                  echo "Found CLUSTER_AGE_KEY, injecting sops-age secret..."
-                  echo "$CLUSTER_AGE_KEY" | kubectl create secret generic sops-age \
-                    --namespace=flux-system \
-                    --from-file=age.agekey=/dev/stdin \
-                    --dry-run=client -o yaml | kubectl apply -f -
+                # If there are no qemu PIDs with talos
+                if [ -z "$QEMU_RUNNING" ]; then
+                  echo "No talos containers found. Creating a new cluster..."
+                  sudo -E talosctl cluster create qemu
+
+                  if [ -z "$GITHUB_TOKEN" ]; then
+                    echo "WARNING: GITHUB_TOKEN not found. flux bootstrap may fail if token-auth is required."
+                  fi
+
+                  echo "Cluster created successfully, initializing FluxCD inside newly created docker cluster"
+                  flux bootstrap github \
+                    --token-auth \
+                    --owner=soriPhoono \
+                    --repository=cluster \
+                    --branch="$(git rev-parse --abbrev-ref HEAD)" \
+                    --path=k8s/clusters/testing/ \
+                    --personal --verbose
+
+                  if [ -n "$CLUSTER_AGE_KEY" ]; then
+                    echo "Found CLUSTER_AGE_KEY, injecting sops-age secret..."
+                    echo "$CLUSTER_AGE_KEY" | kubectl create secret generic sops-age \
+                      --namespace=flux-system \
+                      --from-file=age.agekey=/dev/stdin \
+                      --dry-run=client -o yaml | kubectl apply -f -
+                  else
+                    echo "WARNING: CLUSTER_AGE_KEY not found. SOPS decryption may fail in-cluster."
+                  fi
                 else
-                  echo "WARNING: CLUSTER_AGE_KEY not found. SOPS decryption may fail in-cluster."
+                  echo "Found existing docker cluster, not overwriting..."
                 fi
-              else
-                echo "Found existing docker cluster, not overwriting..."
-              fi
 
-              if talosctl health --wait-timeout 5s >/dev/null 2>&1; then
-                echo "Result: Talos QEMU Cluster is UP and AVAILABLE."
-              else
-                echo "Result: QEMU is running, but the Talos API is UNREACHABLE."
-                exit 1
-              fi
-            '';
-          }}/bin/launch-development-cluster.sh"; };
+                if talosctl health --wait-timeout 5s >/dev/null 2>&1; then
+                  echo "Result: Talos QEMU Cluster is UP and AVAILABLE."
+                else
+                  echo "Result: QEMU is running, but the Talos API is UNREACHABLE."
+                  exit 1
+                fi
+              '';
+            }}/bin/launch-development-cluster.sh";
+          };
         };
 
         treefmt = import ./treefmt.nix {inherit lib pkgs;};
