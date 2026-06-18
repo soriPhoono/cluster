28cc4b4d61ce6a4c575848b80ce48dd277e95324
SoriPhoono <soriphoono@gmail.com>
2026-06-18 00:56:26 -0400

    Ready for reset

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-locals {
-  ghcr_auth_dockerconfigjson = jsonencode({
-    auths = {
-      "ghcr.io" = {
-        username = "flux"
-        password = var.ghcr_pat
-        auth     = base64encode("flux:${var.ghcr_pat}")
-      }
-    }
-  })
-}
-
 module "create-cluster" {
   source = var.debug_mode ? "./terraform/create-dev-cluster" : "./terraform/create-prod-cluster"
 }
@@ -23,16 +11,5 @@ module "flux-operator-bootstrap" {
     instance_yaml = file("${path.root}/k8s/clusters/${var.debug_mode ? "staging" : var.cluster_name}/flux-system/flux-instance.yaml")
   }
 
-  managed_resources = {
-    dockerconfig_yaml = <<-YAML
-    apiVersion: v1
-    kind: Secret
-    metadata:
-      name: flux-dockerconfig
-      namespace: flux-system
-    type: kubernetes.io/dockerconfigjson
-    data:
-      .dockerconfigjson: '${replace(local.ghcr_auth_dockerconfigjson, "'", "''")}'
-    YAML
-  }
+  depends_on = [module.create-cluster]
 }
