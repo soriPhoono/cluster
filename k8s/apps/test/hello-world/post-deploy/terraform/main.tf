4c91a29a63a2831903bce535d1a29410394088e9
SoriPhoono <soriphoono@gmail.com>
2026-06-18 10:14:49 -0400

    Organized post-deploy job

--- k8s/apps/test/hello-world/post-deploy/terraform/main.tf
+++ k8s/apps/test/hello-world/post-deploy/terraform/main.tf
@@ -1,37 +1,3 @@
-terraform {
-  required_providers {
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "~> 4.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-  }
-}
-
-variable "cloudflare_global_api_token" {
-  type      = string
-  sensitive = true
-}
-
-variable "cloudflare_account_id" {
-  type    = string
-  default = "b6e9a277e1c46db22032d0e2c04fb6ae"
-}
-
-variable "cloudflare_domain" {
-  type    = string
-  default = "cryptic-coders.net"
-}
-
-provider "cloudflare" {
-  api_token = var.cloudflare_global_api_token
-}
-
-provider "kubernetes" {}
-
 data "cloudflare_zone" "zone" {
   name = var.cloudflare_domain
 }
